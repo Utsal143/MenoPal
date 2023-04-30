2742f9fc82096165aa4db270c367198fd5c6695f
Utsal Tamrakar <faithlord137@gmail.com>
2023-04-30 21:31:44 +0545

    Calender Design updated

--- lib/screens/Calendar.dart
+++ lib/screens/Calendar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class PeriodCalendar extends StatefulWidget {
-  const PeriodCalendar({super.key});
+  const PeriodCalendar({Key? key}) : super(key: key);
 
   @override
   State<PeriodCalendar> createState() => _PeriodCalendarState();
@@ -13,11 +13,54 @@ class _PeriodCalendarState extends State<PeriodCalendar> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDate;
 
+  List<DateTime> _menstruationDates = []; // List of menstruation dates
+  int _cycleLength = 28; // Average menstrual cycle length is 28 days
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _selectedDate = _focusedDay;
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDate = selectedDay;
+    });
+  }
+
+  void _addMenstruationDate(DateTime date) {
+    setState(() {
+      _menstruationDates.add(date);
+    });
+  }
+
+  void _removeMenstruationDate(DateTime date) {
+    setState(() {
+      _menstruationDates.remove(date);
+    });
+  }
+
+  DateTime _getNextCycleStartDate() {
+    if (_menstruationDates.isEmpty) {
+      return _focusedDay.add(Duration(days: _cycleLength));
+    } else {
+      DateTime lastMenstruationDate = _menstruationDates.last;
+      return lastMenstruationDate.add(Duration(days: _cycleLength));
+    }
+  }
+
+  void _scheduleNotification(DateTime notificationDate) {
+    // TODO: Implement notification scheduling
+  }
+
+  List<DateTime> _getMenstruationDaysInRange(DateTime start, DateTime end) {
+    List<DateTime> menstruationDays = [];
+    for (DateTime date in _menstruationDates) {
+      if (date.isAfter(end)) break;
+      if (date.isBefore(start)) continue;
+      menstruationDays.add(date);
+    }
+    return menstruationDays;
   }
 
   @override
@@ -25,12 +68,55 @@ class _PeriodCalendarState extends State<PeriodCalendar> {
     return Scaffold(
       appBar:
           AppBar(backgroundColor: Colors.pink, title: Text("Period Calendar")),
-      body: Column(children: [
-        TableCalendar(
+      body: Column(
+        children: [
+          TableCalendar(
             focusedDay: _focusedDay,
             firstDay: DateTime(2020),
-            lastDay: DateTime(2030))
-      ]),
+            lastDay: DateTime(2030),
+            calendarFormat: _calendarFormat,
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDate, day);
+            },
+            onDaySelected: _onDaySelected,
+            //eventLoader: _getMenstruationDaysInRange,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () => _addMenstruationDate(_selectedDate!),
+                child: Text(
+                  "Log Menstruation Day",
+                  style: TextStyle(fontSize: 13),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => _removeMenstruationDate(_selectedDate!),
+                child: Text(
+                  "Remove Menstruation Day",
+                  style: TextStyle(fontSize: 13),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Text(
+              "Next cycle starts on: ${_getNextCycleStartDate().toString().substring(0, 10)}"),
+          ElevatedButton(
+            onPressed: () => _scheduleNotification(_getNextCycleStartDate()),
+            child: Text(
+              "Set Reminder",
+              style: TextStyle(fontSize: 13),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
